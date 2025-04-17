9181b7318d556cc3162d7e8f5584eaa6c3ce0069
4hqld <hhb107@naver.com>
2025-04-17 13:22:37 +0900

    modified:   AutoInstaller.jar 	new file:   Tree.ini 	modified:   build.bat 	new file:   build/JSON/accounts.json 	new file:   build/JSON/install_list.json 	deleted:    build/autoinstaller/Account.class 	new file:   build/autoinstaller/Accounts/AccountsManager$1.class 	new file:   build/autoinstaller/Accounts/AccountsManager$2.class 	new file:   build/autoinstaller/Accounts/AccountsManager$3.class 	new file:   build/autoinstaller/Accounts/AccountsManager.class 	new file:   build/autoinstaller/Accounts/Admin/Admin.class 	new file:   build/autoinstaller/Accounts/LoginManager$1.class 	new file:   build/autoinstaller/Accounts/LoginManager.class 	deleted:    build/autoinstaller/Admin.class 	deleted:    build/autoinstaller/AuthManager$1.class 	deleted:    build/autoinstaller/AuthManager.class 	modified:   build/autoinstaller/Installer.class 	modified:   build/autoinstaller/Main.class 	deleted:    build/autoinstaller/Program.class 	deleted:    build/autoinstaller/Utils.class 	new file:   build/autoinstaller/Utils/Program.class 	renamed:    build/autoinstaller/ProgramInstallResult.class -> build/autoinstaller/Utils/ProgramInstallResult.class 	new file:   build/autoinstaller/Utils/Utils.class 	deleted:    manifest.txt

--- build.bat
+++ build.bat
@@ -1,59 +1,62 @@
 @echo off
 setlocal
 
-:: [정보] 경로 설정
-set SRC_DIR=src
-set BUILD_DIR=build
-set LIB_DIR=lib
-set MAIN_CLASS=autoinstaller.Main
-set JAR_NAME=AutoInstaller-FAT.jar
+:: ???? ??
+set SRC=src
+set BUILD=build
+set LIB=lib
+set JSON=JSON
+set JAR_NAME=AutoInstaller.jar
 
-:: [정보] build 폴더 초기화
-echo build 디렉터리 초기화 중...
-if exist %BUILD_DIR% rmdir /s /q %BUILD_DIR%
-mkdir %BUILD_DIR%
+:: ?? ???? ???
+if exist %BUILD% (
+    echo [Info] Initializing...
+    rmdir /s /q %BUILD%
+)
+mkdir %BUILD%
 
-:: [정보] 클래스 컴파일
-echo 소스 컴파일 중...
-javac -d %BUILD_DIR% -cp "%LIB_DIR%\*" %SRC_DIR%\autoinstaller\*.java
+:: ????? ?? (????? ??)
+set CLASSPATH=%LIB%\jackson-annotations-2.15.2.jar;%LIB%\jackson-core-2.15.2.jar;%LIB%\jackson-databind-2.15.2.jar
 
-if %ERRORLEVEL% NEQ 0 (
-    echo 컴파일 실패.
-    exit /b 1
+:: ?? Java ?? ??? ??
+echo Finding Sources...
+del sources.txt >nul 2>&1
+for /r %SRC% %%f in (*.java) do (
+    echo %%f >> sources.txt
 )
 
-:: [정보] fat jar용 임시 폴더 생성
-set TMP_DIR=tmp_jar
-if exist %TMP_DIR% rmdir /s /q %TMP_DIR%
-mkdir %TMP_DIR%
-mkdir %TMP_DIR%\autoinstaller
+:: ???
+echo Loading...
+javac -cp %CLASSPATH% -d %BUILD% @sources.txt
 
-:: [정보] 클래스 복사
-xcopy /Y /E %BUILD_DIR%\*.class %TMP_DIR%\
-
-:: [정보] 라이브러리 JAR 압축 해제
-echo 라이브러리 압축 해제 중...
-for %%f in (%LIB_DIR%\*.jar) do (
-    echo    %%~nxf 포함 중...
-    pushd %TMP_DIR%
-    jar xf ..\%%f
-    popd
+if not %ERRORLEVEL%==0 (
+    echo Failed!
+    del sources.txt >nul 2>&1
+    pause
+    exit /b
 )
 
-:: [정보] fat jar 생성
-echo Main-Class: %MAIN_CLASS% > manifest.txt
-jar cfm %JAR_NAME% manifest.txt -C %TMP_DIR% .
+:: ??? ?? ? ?? ?? ??? ??
+del sources.txt >nul 2>&1
+echo Success!
 
-:: [정리]
-rmdir /s /q %TMP_DIR%
+:: Manifest ?? ??
+echo Main-Class: Main> manifest.txt
 
-echo fat JAR 생성 완료: %JAR_NAME%
+:: ??? ???(JSON ?) ??
+xcopy %JSON% %BUILD%\JSON\ /E /I /Y >nul
 
-:: [정보] 실행 예시
-echo.
-echo 실행 명령어:
-echo java -jar %JAR_NAME%
-echo.
+:: JAR ??
+echo Generating jar file...
+jar cfm %JAR_NAME% manifest.txt -C %BUILD% .
 
+if %ERRORLEVEL%==0 (
+    echo [Success] %JAR_NAME% Generated!
+) else (
+    echo [Error] JAR Failed!
+)
+
+:: ??
+del manifest.txt >nul 2>&1
 endlocal
 pause
